9fc9771a69350a6fb4f8e8578a4385f5f508794c
Mo <mo@gmail.com>
2024-03-24 22:46:07 +0200

    first commit

---
+++ Even and Odd.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+
+	int Number;
+	cin >> Number;
+	if (Number % 2 == 0) {
+		cout << "Number is Even " << endl;
+	}
+	else {
+		cout << "Number is Odd " << endl;
+	}
+}
